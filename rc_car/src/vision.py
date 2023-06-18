5b62beb0e945ef5bad9e5251beca9706781ca994
cold_rice <80147216+micromielec@users.noreply.github.com>
2023-06-19 00:14:06 +0900

    Update vision.py

--- rc_car/src/vision.py
+++ rc_car/src/vision.py
@@ -17,10 +17,10 @@ def cvpub():
     hsv = cv2.cvtColor(img_color, cv2.COLOR_BGR2HSV)    # BGR을 HSV로 변환해줌
 
     # define range of blue color in HSV
-    lower_red = np.array([0,100,120])          # 파랑색 범위
+    lower_red = np.array([0,100,120])          # 빨간색 범위
     upper_red = np.array([10,255,255])
     img_mask = cv2.inRange(hsv, lower_red, upper_red)     # 110<->150 Hue(색상) 영역을 지정.
-    res = cv2.bitwise_and(img_color, img_color, mask=img_mask)      # 흰색 영역에 파랑색 마스크를 씌워줌.
+    res = cv2.bitwise_and(img_color, img_color, mask=img_mask)      # 흰색 영역에 빨간색 마스크를 씌워줌.
 
     if img_mask.any()== True:
     	print("stop")
@@ -33,7 +33,7 @@ def cvpub():
     # Bitwise-AND mask and original image
     pub = rospy.Publisher('cv_detection', String, queue_size=10)
     cv2.imshow('frame',img_color)       # 원본 영상을 보여줌
-    cv2.imshow('Red', res)           # 마스크 위에 파랑색을 씌운 것을 보여줌.
+    cv2.imshow('Red', res)           # 마스크 위에 빨간색을 씌운 것을 보여줌.
     if img_mask.any()== True:
         print("stop")
         pub.publish("RED")
